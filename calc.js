49af0a7cf6fb444a3a4ae0be0d77ca0bdd09b00d
ashleycodes920 <59901528+ashleycodes920@users.noreply.github.com>
2020-10-02 09:14:15 -0500

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,11 @@
+//var age = prompt("How old are you?");
+//var days = age * 365
+//console.log(age + " years is roughly " + days + " days")
+
+//Age Calculator
+//Enter your age and find out how many days you've been alive
+//to times age for leap year you add .25 to 365
+
+var age = prompt("How old are you?");
+var days = age * 365.25
+console.log(age + " years is roughly " + days + " days")
